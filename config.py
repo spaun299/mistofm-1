2e309748a8da6ebb2fb690fecc15c100b075ff20
Viktor Kobryn <vkobryn@lohika.com>
2017-09-26 16:59:30 +0300

    init app, connect to db, configure logger

---
+++ config.py
@@ -0,0 +1,10 @@
+import secret_data
+
+DEBUG = True
+DB_NAME = secret_data.DB_NAME
+DB_HOST = secret_data.DB_HOST
+DB_USERNAME = secret_data.DB_USERNAME
+DB_PASSWORD = secret_data.DB_PASSWORD
+LOG_PATH = "mistofm.log"
+# LOG_PATH = "/var/log/mistofm.log"
+LOG_ROTATE_COUNT = 5
