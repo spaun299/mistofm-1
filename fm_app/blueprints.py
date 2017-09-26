345c81baac9ae867a839377dbd9afc7ebb82eaaf
Viktor Kobryn <vkobryn@lohika.com>
2017-09-26 12:43:48 +0300

    move static files to proper folders

--- fm_app/blueprints.py
+++ fm_app/blueprints.py
@@ -2,8 +2,10 @@ from flask.blueprints import Blueprint
 
 
 index_blueprint = Blueprint('index', __name__)
+static_blueprint = Blueprint('static', __name__, static_folder='static')
 
 
 def register_blueprints(app):
     from .views import index_view
     app.register_blueprint(index_blueprint, url_prefix='')
+    app.register_blueprint(static_blueprint)
