26a26db1b6220df4de5caffd8bbfa8e9268f1f4d
Vovax <vkhves@icloud.com>
2017-10-19 22:59:34 +0300

    add playlist

--- fm_app/static/js/player.js
+++ fm_app/static/js/player.js
@@ -1,4 +1,5 @@
 var audio = document.getElementById("audio-player");
+var metadataUrl;
 
 $(document).ready(function() {
 
@@ -64,7 +65,7 @@ $(document).ready(function() {
     $(".fb-share").on('click', function() {
         cache: false;
         var name = $('#onListen').text();
-        var image = "http://127.0.0.1:5000/static/img/logo_400x400.png";
+        var image = "http://mistofm.com.ua/static/img/logo_400x400.png";
 
         if(name!='Misto FM') {
                            
@@ -72,22 +73,56 @@ $(document).ready(function() {
 
         name = encodeURIComponent(name);
         image = encodeURIComponent(image);
-        if (image === 'undefined') { image = "http://127.0.0.1:5000/static/img/logo_400x400.png"; }
+        if (image === 'undefined') { image = "http://mistofm.com.ua/static/img/logo_400x400.png"; }
 
         var ttt = "https://www.facebook.com/dialog/feed?app_id=535111550164465&display=popup&caption="+encodeURIComponent("Misto Fm ● Lviv ● Deep ")+"&link=http://mistofm.com.ua/";
-        var link_text = ttt+"&name="+"&description="+"&picture="+image;
+        var link_text = ttt+"&name="+"&picture="+image;
                         
         window.open(link_text,'popup', 'width=640,height=520')
     });
-    
-    
-
-
 
 	$('#np-volume').on('input propertychange', function() {
 		var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
         audio.volume = val;
 	});
+
+	(function radioTitle() {
+
+        var songContent;
+        $.ajaxSetup({
+            scriptCharset: "utf-8",
+            contentType: "application/json; charset=utf-8"
+        })
+
+        $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://mistofm.com.ua:8777/metadata/get/3/') + '&callback=?', function(data){})
+        .done(function(data) {
+            songContent = JSON.parse(data.contents);
+            $("#onListen").empty();
+            $(".playlist").empty();
+
+            $($(songContent).get().reverse()).each( function(n, songJson) {
+                if (n == 0) {
+                    var contentPlayNow = '<span>' + songJson.name + '</span>';
+                    var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from +'</span><span class="about-name on-listen">' + songJson.name +'</span></li>';
+                    $(contentPlayList).appendTo(".playlist");
+                    $(contentPlayNow).appendTo("#onListen");
+                } else {
+                    var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from +'</span><span class="about-name on-listen">' + songJson.name +'</span></li>';
+                    $(contentPlayList).appendTo(".playlist");
+               }
+            });
+        })
+        .always(function(data) {
+            var nextUpdate = data.next_update;
+            if (!nextUpdate) {
+                nextUpdate = 36;
+            }
+            setTimeout(function() {
+                radioTitle();
+            }, nextUpdate * 1000);
+        });
+
+    })();
 
 
 });
