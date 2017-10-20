6d5e23d51a4323b51e2666cc01f2b2feefd70d20
spaun299 <spaun1002@gmail.com>
2017-10-20 19:18:35 +0300

    fix bug with head and error html

--- fm_app/static/js/player.js
+++ fm_app/static/js/player.js
@@ -4,27 +4,27 @@ var metadataUrl;
 $(document).ready(function() {
 
     var player = $('.player');
-    
+
     player.on('click', '.btn-player', function(e){
-    	e.preventDefault();
-    	if ( $(this).hasClass('play') ) {
-    		$(this).removeClass('play');
-    		$(this).addClass('pause');
-    		audio.play();
+        e.preventDefault();
+        if ( $(this).hasClass('play') ) {
+                $(this).removeClass('play');
+                $(this).addClass('pause');
+                audio.play();
             // audio.currentTime = 0;
-    	} else {
-    		$(this).removeClass('pause');
-    		$(this).addClass('play');
-    		
-    		audio.pause();
+        } else {
+                $(this).removeClass('pause');
+                $(this).addClass('play');
+
+                audio.pause();
             // audio.currentTime = 0;
-    	}
+        }
     });
-    
+
     /***********************************************************************************************/
     /* SEARCH TRACK */
     /***********************************************************************************************/
-    
+
     // YOUTUBE-----
     $(".utube-find").on('click', function() {
         cache: false;
@@ -32,21 +32,21 @@ $(document).ready(function() {
         var image = $('#track-image img').attr('src');
 
         if(name!='Misto FM') {
-                            
+
         }
 
         name = encodeURIComponent(name);
         image = encodeURIComponent(image);
         if (image === 'undefined') { image = 'http://reold-vovax.c9users.io/img/misto_logo.png'; }
 
-        var ttt = "https://www.youtube.com/results?search_query=";  
+        var ttt = "https://www.youtube.com/results?search_query=";
         var link_text = ttt+name;
-        
+
         window.open(link_text,'_blank')
 
     });
-    
-    // SOUNDCLOUD-----
+
+  // SOUNDCLOUD-----
     $(".scloud-find").on('click', function() {
         cache: false;
         var name = $('#onListen').text();
@@ -55,12 +55,12 @@ $(document).ready(function() {
 
         var ttt = "https://soundcloud.com/search/sounds?q=";
         var link_text = ttt+name;
-        
+
         window.open(link_text,'_blank')
 
     });
-    
-    
+
+
     // FACEBOOK-----
     $(".fb-share").on('click', function() {
         cache: false;
@@ -68,7 +68,7 @@ $(document).ready(function() {
         var image = "http://mistofm.com.ua/static/img/logo_400x400.png";
 
         if(name!='Misto FM') {
-                           
+
         }
 
         name = encodeURIComponent(name);
@@ -77,27 +77,29 @@ $(document).ready(function() {
 
         var ttt = "https://www.facebook.com/dialog/feed?app_id=535111550164465&display=popup&caption="+encodeURIComponent("Misto Fm ● Lviv ● Deep ")+"&link=http://mistofm.com.ua/";
         var link_text = ttt+"&name="+"&picture="+image;
-                        
+
         window.open(link_text,'popup', 'width=640,height=520')
     });
-
-	$('#np-volume').on('input propertychange', function() {
-		var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+        $('#np-volume').on('input propertychange', function() {
+                var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
         audio.volume = val;
-	});
+        });
 
-	(function radioTitle() {
+        (function radioTitle() {
+
+        var songContent;
         $.ajaxSetup({
             scriptCharset: "utf-8",
             contentType: "application/json; charset=utf-8"
         })
 
-        $.getJSON(metadataUrl), function(data){}
+        $.getJSON(metadataUrl, function(data){})
         .done(function(data) {
+            songContent = data;
             $("#onListen").empty();
             $(".playlist").empty();
 
-            $($(data).get().reverse()).each( function(n, songJson) {
+            $($(songContent).get().reverse()).each( function(n, songJson) {
                 if (n == 0) {
                     var contentPlayNow = '<span>' + songJson.name + '</span>';
                     var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from + '</span><span class="about-name on-listen">' + songJson.name + '</span></li>';
@@ -123,4 +125,3 @@ $(document).ready(function() {
 
 
 });
-
