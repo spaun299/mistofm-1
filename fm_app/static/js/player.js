08014f41e685c19fb4e1bc4867c9d353f556520a
Vovax <vkhves@icloud.com>
2017-10-20 08:07:22 +0300

    playlist fix vs @media & site analytics

--- fm_app/static/js/player.js
+++ fm_app/static/js/player.js
@@ -94,7 +94,7 @@ $(document).ready(function() {
             contentType: "application/json; charset=utf-8"
         })
 
-        $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://mistofm.com.ua:8777/metadata/get/3/') + '&callback=?', function(data){})
+        $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(metadataUrl) + '&callback=?', function(data){})
         .done(function(data) {
             songContent = JSON.parse(data.contents);
             $("#onListen").empty();
@@ -103,11 +103,11 @@ $(document).ready(function() {
             $($(songContent).get().reverse()).each( function(n, songJson) {
                 if (n == 0) {
                     var contentPlayNow = '<span>' + songJson.name + '</span>';
-                    var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from +'</span><span class="about-name on-listen">' + songJson.name +'</span></li>';
+                    var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from + '</span><span class="about-name on-listen">' + songJson.name + '</span></li>';
                     $(contentPlayList).appendTo(".playlist");
                     $(contentPlayNow).appendTo("#onListen");
                 } else {
-                    var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from +'</span><span class="about-name on-listen">' + songJson.name +'</span></li>';
+                    var contentPlayList = '<li class="about-list clearfix"><span class="about-icon">' + songJson.play_from + '</span><span class="about-name on-listen">' + songJson.name + '</span></li>';
                     $(contentPlayList).appendTo(".playlist");
                }
             });
